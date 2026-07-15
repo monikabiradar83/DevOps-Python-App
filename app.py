ea71d98745a39839629f7bc76a5efa9d4dd6169d
Monika Biradar <monikabiradar295@gmail.com>
2026-07-15 16:56:51 +0530

    updated home page message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps! Welcome to My First Flask Application."
+    return "CI/CD Pipeline Working Successfully!"
 
 @app.route('/about')
 def about():
